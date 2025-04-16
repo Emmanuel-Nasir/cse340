488de7188113961f363a4db33c660e155d8b558e
Emmanuel-Nasir <nas24011@byui.edu>
2025-04-16 23:46:54 +0100

    commit wk3 and wk4

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -13,13 +13,12 @@ async function buildLogin(req, res, next) {
   });
 }
 async function buildRegister(req, res, next) {
+  let nav=await utilities.getNav()
   res.render("account/register", {
     title: "Register",
-    notice: null, 
-    errors: null, 
-    account_firstname:"",
-    account_lastname:"",
-    account_email: ""
+    nav, 
+    errors: null,
+    message:null 
   })
 }
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,4 +7,4 @@ const utilities = require("../utilities")
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-module.exports = router;
+module.exports = router
